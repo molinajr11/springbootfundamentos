a627f65fd573f458a7788602936bb06becc08066
molinajr11 <molinajunior1111@gmail.com>
2023-06-30 17:58:57 -0500

    uso del @Value para inyectar valores de configuracion se puede usar en controladores servicios o componentes

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
@@ -22,6 +24,15 @@ public class GeneralConfiguration {
 
     @Value("${value.random}")
     private String random;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
 
     @Bean
     public MybeanProperties function(){
